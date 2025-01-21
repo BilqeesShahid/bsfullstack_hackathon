dd154f106f5dee677adb8aeae77cbfeb0a945bfa
Bilqees Shahid <161162316+BilqeesShahid@users.noreply.github.com>
2025-01-22 04:46:48 +0500

    Update page.tsx

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -2,7 +2,7 @@
 import Container from "@/components/Container";
 import BestSell from "../../components/BestSell";
 import Hero from "../../components/Hero";
-import { getAllProducts,  getAllCategories,getHero} from "@/sanity/helpers";
+import { getAllCategories,getHero} from "@/sanity/helpers";
   
 import Featured from "@/components/Featured";
 import DontMiss from "@/components/DontMiss";
